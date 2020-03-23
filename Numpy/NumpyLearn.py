bc3a2e2ae49d9f42a7c874abd4282b8fb13c58a4
lizyhbu <77211356@qq.com>
2020-03-23 23:27:00 +0800

    numpy where 替换指定阈值

--- Numpy/NumpyLearn.py
+++ Numpy/NumpyLearn.py
@@ -1,5 +1,9 @@
 import numpy as np
 vector = np.array([1, 2, 3, 4])
 metrix = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
-print(vector)
-print(metrix)
+
+result=np.where(metrix < 5, 0, metrix)
+result=np.where(result >= 5, 1, result)
+print(result)
+result2=np.where(metrix >= 5, 1, 0)
+print(result2)
